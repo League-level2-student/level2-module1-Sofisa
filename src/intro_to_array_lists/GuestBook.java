56a539934d0e7162e6343f6a1adcfca24c490178
Sofisa <league@WTS10.attlocal.net>
2018-09-09 17:30:42 -0700

    GuestBook(IP)

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,44 @@
 package intro_to_array_lists;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GuestBook {
+	void createGUI() {
+	JFrame frame = new JFrame();
+	frame.setVisible(true);
+	JButton addName = new JButton();
+	JButton view = new JButton();
+	addName.setText("Add Name");
+	view.setText("View Names");
+	JPanel panel = new JPanel();
+	frame.add(panel);
+	panel.add(addName);
+	panel.add(view);
+	frame.pack();
+	 
+		
+	}
+	public static void main(String[] args) {
+		GuestBook g = new GuestBook();
+		g.createGUI();
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
+	
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+	
 	// all the names added to the list. Format the list as follows:
+	
 	// Guest #1: Bob Banders
+	
 	// Guest #2: Sandy Summers
+	
 	// Guest #3: Greg Ganders
+	
 	// Guest #4: Donny Doners
+	
 }
+
